a0bfafefe95a096de7f6cc8de72c65bc5f7c17b7
Martin Juul <code@juul.xyz>
2018-06-13 16:54:44 +0200

    Cleaned up logging labels

--- src/sockets.ts
+++ src/sockets.ts
@@ -15,18 +15,18 @@ export class Sockets
 
         chatRooms.forEach(room => {
             const listenRoom = this._sio.of(room.id);
-            console.log(`[socket][${room.label}]: started listening`);
+            console.log(`[socket][chatRoom](${room.label}): started listening`);
 
             listenRoom.on('connect', (socket => {
-                console.log(`[socket][${room.label}]: connected client`);
+                console.log(`[socket][chatRoom](${room.label}): connected client`);
 
                 socket.on('message', (m: Message) => {
-                    console.log(`[server][${room.label}](message): ${JSON.stringify(m)}`);
+                    console.log(`[server][chatRoom][${room.label}](message): ${JSON.stringify(m)}`);
                     listenRoom.emit('message', m);
                 });
 
                 socket.on('disconnect', () => {
-                    console.log(`[server][${room.label}]: Client disconnected`);
+                    console.log(`[server][chatRoom](${room.label}): Client disconnected`);
                 });
             }));
         });
